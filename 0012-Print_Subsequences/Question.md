14b1440239f2b7ee293480349a32f2973b72f54c
AryanKanade <aryankanade69@gmail.com>
2026-07-21 14:12:49 +0530

    Easiest way to find all Subsequences and Unique Subsequences

--- 0012-Print_Subsequences/Question.md
+++ 0012-Print_Subsequences/Question.md
@@ -1,1 +1,3 @@
-Print all the Subsequences of a string "abc"
+1.Print all the Subsequences of a string "abc"
+
+2.Print all the Unique Subsequences of a string "aaa"
--- 0012-Print_Subsequences/Solution.java
+++ 0012-Print_Subsequences/Solution.java
@@ -1,18 +1,53 @@
+import java.util.*;
+
+// 1.
+
+// public class Solution {
+//     public static void subSequences(String str, int idx, String newString){
+//         if(idx == str.length()){
+//             System.out.println(newString);
+//             return;
+//         }
+
+//         char currChar = str.charAt(idx);
+
+//         subSequences(str, idx+1, newString+currChar);
+//         subSequences(str, idx+1, newString);
+//     }
+
+//     public static void main(String args[]){
+//         String str = "abc";
+//         subSequences(str, 0, "");
+//     }
+// }
+
+
+// 2.
+// For Unique SubSequence
+
 public class Solution {
-    public static void subSequences(String str, int idx, String newString){
+    public static void subSequences(String str, int idx, String newString, HashSet<String> set){
         if(idx == str.length()){
-            System.out.println(newString);
-            return;
+            if(set.contains(newString)){
+                return;
+            }else{
+                System.out.println(newString);
+                set.add(newString);
+                return;
+            }
         }
 
         char currChar = str.charAt(idx);
+        //Include Character
+        subSequences(str, idx+1, newString+currChar, set);
 
-        subSequences(str, idx+1, newString+currChar);
-        subSequences(str, idx+1, newString);
+        //Do not Include Character
+        subSequences(str, idx+1, newString, set);
     }
 
     public static void main(String args[]){
-        String str = "abc";
-        subSequences(str, 0, "");
+        HashSet<String> set = new HashSet<>();
+        String str = "aaa";
+        subSequences(str, 0, "", set);
     }
 }

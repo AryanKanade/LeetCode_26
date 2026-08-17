2019bfde84910232a37b61d5448fcc51249a4c77
AryanKanade <aryankanade69@gmail.com>
2026-08-17 07:40:26 +0530

    Solved 2 sum using 2 pointers

---
+++ 0015-Two_Sum_Sorted/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int right = nums.length-1;
+        int left = 0;
+        while(left<right){
+            int sum = nums[left] + nums[right];
+            if(sum == target){
+                return new int[]{left, right};
+            }else if(sum>target){
+                right--;
+            }else{
+                left++;
+            }
+        }
+        return new int[]{};
+    }
+}

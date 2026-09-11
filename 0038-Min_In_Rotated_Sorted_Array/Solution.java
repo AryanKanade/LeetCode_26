9381c4bd5dfa1350d4919a587ddb5f378a3bc0cf
AryanKanade <aryankanade69@gmail.com>
2026-09-11 15:55:57 +0530

    Here we are taking the low from sorted side and then discarding that side

---
+++ 0038-Min_In_Rotated_Sorted_Array/Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        int low = 0;
+        int high = nums.length-1;
+        int mid = 0;
+        while(low <= high){
+            mid = low + (high - low)/2;
+            if(nums[low]<=nums[high]){
+                min = Math.min(min,nums[low]);
+                break;
+            }
+
+            if(nums[low] <= nums[mid]){
+                min = Math.min(min,nums[low]);
+                low = mid+1;
+            }
+            else{
+                min = Math.min(min,nums[mid]);
+                high = mid-1; 
+            }
+        }
+        return min;
+    }
+}
